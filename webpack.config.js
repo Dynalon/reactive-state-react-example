27696cf32e3034927c9e760250c7092c9e7455b9
Timo Dörr <timo@latecrew.de>
2018-08-07 11:24:45 +0200

    Bump webpack to v4 + bump its deps

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         extensions: [".ts", ".tsx", ".js", ".jsx"],
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.tsx?$/,
                 loader: "awesome-typescript-loader"
@@ -29,6 +29,7 @@ module.exports = {
     node: {
         Buffer: false
     },
+    mode: "development",
     devtool: "source-map",
     context: __dirname,
     target: "web",
